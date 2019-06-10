cf8adaece73a96ef11fc3964f38894a279bc3286
Jason Chen <jason.sl.chen@gmail.com>
2019-06-09 22:11:02 -0700

    work on part1.js testing suite and underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length - 1];
+    } else if (n > array.length) {
+      return array;
+    } else {
+      return array.slice([array.length - n]);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +53,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (let val in collection) {
+        iterator(collection[val], val, collection);
+      }
+    }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,12 +84,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    let pass = [];
+    for (let i = 0; i < collection.length; i++) {
+      if (test(collection[i])) {
+        pass.push(collection[i]);
+      }
+    }
+    return pass;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(value) {
+      return !(test(value));
+    });
   };
 
   // Produce a duplicate-free version of the array.
